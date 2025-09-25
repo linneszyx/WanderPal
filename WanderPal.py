67c2fb1ea497bc35e247ef55e953a4ada2d5f59b
linneszyx <itssuryanshcooldude346@gmail.com>
2025-09-25 10:47:14 +0530

    adding constants and utils

--- WanderPal.py
+++ WanderPal.py
@@ -13,11 +13,12 @@ def load_secrets():
     env_path = Path(".")/".env"
     load_dotenv(dotenv_path=env_path)
 
-    open_ai_key = os.getenv("OPENAI_API_KEY")
+    open_ai_key = os.getenv("OPENAI_API_KEY")   
     google_maps_key = os.getenv("GOOGLE_MAPS_API_KEY")
     google_palm_key = os.getenv("GOOGLE_PALM_API_KEY")
 
     return {
         "OPEN_API_KEY" : open_ai_key,
-        "GOOGLE_PALM_API_KEY" : google_palm_key
+        "GOOGLE_PALM_API_KEY" : google_palm_key,
+
     }
